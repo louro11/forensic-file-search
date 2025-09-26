0a95e94d055dbf327dfe87245e6883a8fbe1b72d
Goncalo <goncalo_louro@hotmail.com>
2025-09-26 11:14:49 +0100

    changes to file hash calculation

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,38 @@
 use walkdir::WalkDir;
-use clap::Parser;
 use::std;
 use std::path::{self, Path};
 use std::fs::File;
-use std::io::{self, Read}; // <- make sure Read is imported
+use std::io::{self, BufRead, BufReader};
+use std::io::{Read}; // <- make sure Read is imported
 use std::io::{BufWriter, Write};
 use chrono::Local;
 use sha1::{Sha1, Digest as Sha1Digest};
 use sha2::{Sha256, Digest as Sha256Digest};
-use hex_literal::hex;
+use std::collections::HashSet;
 
 
 fn main() -> std::io::Result<()>{
 
 
+    println!("Insert path to scan: ");
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    let path_to_scan = input.trim();
+    println!("Scanning the following path, {}", path_to_scan);
+
     //create if not exists config file
     let config_path = "config.txt";
     if !Path::new(config_path).exists() {
         let mut config_file = File::create(config_path)?;
-        writeln!(config_file, "[Drives to scan]")?;
-        writeln!(config_file, "C:\\")?;
         writeln!(config_file, "[File extensions]")?;
         writeln!(config_file, "pdf,doc,docx,xls,xlsx,ppt,pptx")?;
         writeln!(config_file, "[Keywords]")?;
-        writeln!(config_file, "-")?;
+        writeln!(config_file, "One pear line")?;
+        writeln!(config_file, "[Skip the following paths]")?;
+        writeln!(config_file, "C:\\Windows")?;
         println!("Config file created at {}", config_path);
     } else {
         println!("Loading existing config file {}", config_path);
@@ -32,8 +41,8 @@ fn main() -> std::io::Result<()>{
     let timestamp = Local::now().format("%d-%m-%Y_%H-%M-%S");
 
 
-    let mut targets:Vec<String> = Vec::new();
-    targets.push("D:\\03_PROJETOS\\FORENSIC FILES TARGET".to_string());
+    //let mut targets:Vec<String> = Vec::new();
+    //targets.push(path);
     
     let file_extensions = ["pdf", "doc", "docx", "xls", "xlsx", "ppt", "pptx"];
 
@@ -42,57 +51,89 @@ fn main() -> std::io::Result<()>{
     let mut result_number = 0;
     let mut compress_files_number = 0;
 
+    //Initiate report
     let filename = format!("{}-report.txt", timestamp);
     let output_file = File::create(&filename)?;
     let mut writer = BufWriter::new(output_file);
-    for target in targets{
-        for entry in WalkDir::new(target).into_iter().filter_map(|e|e.ok()){
-            if let Some(ext) = entry.path().extension(){
+    writeln!(writer, "Forensic file search report")?; 
+    writeln!(writer, " ")?; 
 
-                if file_extensions.iter().any(|wanted|ext.eq_ignore_ascii_case(wanted)){
-                    
-                    let path = entry.path();
-                    let mut entry_file = File::open(path)?;
-                    result_number+=1;
-                    writeln!(writer, "---")?; 
+    //for target in targets{
+    for entry in WalkDir::new(path_to_scan).into_iter().filter_map(|e|e.ok()){
+        if let Some(ext) = entry.path().extension(){
 
+            if file_extensions.iter().any(|wanted|ext.eq_ignore_ascii_case(wanted)){
+                
+                let path = entry.path();
+                let entry_file = File::open(path)?;
+                let mut reader = BufReader::new(entry_file);
+                
+                //Prepare hashes
+            
+                let mut sha1_hasher = Sha1::new();
+                let mut sha256_hasher = Sha256::new();
 
-                    //check for keyword
+                //Prepare keywords
+                let keywords: HashSet<&str> = ["anexo"].into_iter().collect();
+                let mut keyword_results = Vec::new();
 
-                    
-                    //Calculate hashes
-                    // SHA-1
-                    let mut sha1_hasher = Sha1::new();
-                    // SHA-256
-                    let mut sha256_hasher = Sha256::new();
-
-                    let mut buffer = [0u8; 8192];
-                    loop {
-                        let n = entry_file.read(&mut buffer)?;
-                        if n == 0 { break; }
-                        sha1_hasher.update(&buffer[..n]);
-                        sha256_hasher.update(&buffer[..n]);
-                    }
-
-                    let sha1_result = sha1_hasher.finalize();
-                    let sha256_result = sha256_hasher.finalize();
-
-                    // Write results to file
-                    writeln!(writer, "{}", entry.path().display())?; 
-                    writeln!(writer, "SHA-1   : {:x}", sha1_result)?;
-                    writeln!(writer, "SHA-256 : {:x}", sha256_result)?;
-                    writeln!(writer, "keyword snippet: ");
-                    writeln!(writer, "---")?; 
+                //Check filename
+                let filename = path.file_name().unwrap().to_string_lossy().to_lowercase();
+                if keywords.iter().any(|k| filename.contains(k)) {
+                    println!("Keyword found in file name: {}", path.display());
                 }
 
-                if compressed_file_extensions.iter().any(|compressed|ext.eq_ignore_ascii_case(compressed)){
-                    compressed_files_handler(entry.path());
-                    compress_files_number+=1;
-                }  
-            }       
-        }
+                //Prepare buffer for reading
+                let mut buf = Vec::new();
+                while reader.read_until(b'\n', &mut buf)? != 0 {
+                    // update hashers with raw bytes
+                    sha1_hasher.update(&buf);
+                    sha256_hasher.update(&buf);
 
-    } 
+                    // Convert buf to an owned String
+                    let line_owned = match String::from_utf8(buf.clone()) {
+                        Ok(s) => s,
+                        Err(_) => {
+                            buf.clear();
+                            continue; // skip lines that are not valid UTF-8
+                        }
+                    };
+                    
+                    let line_clean = line_owned.clone();
+                    for word in line_clean.trim_end().split(|c: char| !c.is_alphanumeric()) {
+                        if keywords.contains(word) {
+                                keyword_results.push((word.to_string(), line_clean.clone()));
+                                result_number+=1;
+                                //break;
+                        }
+                    }
+
+                    buf.clear(); // reset buffer for next line
+
+                }
+
+                let sha1_result = sha1_hasher.finalize();
+                let sha256_result = sha256_hasher.finalize();
+
+                // Write results to file
+                writeln!(writer, "Path: {}", entry.path().display())?; 
+                writeln!(writer, "SHA-1   : {:x}", sha1_result)?;
+                writeln!(writer, "SHA-256 : {:x}", sha256_result)?;
+
+                for keyword_res in keyword_results{
+                    writeln!(writer, "Keyword hit: '{}' in line: '{}'", keyword_res.0, keyword_res.1)?;
+                }
+                
+                writeln!(writer, "---")?; 
+            }
+
+            if compressed_file_extensions.iter().any(|compressed|ext.eq_ignore_ascii_case(compressed)){
+                compressed_files_handler(entry.path());
+                compress_files_number+=1;
+            }  
+        }       
+    }
+
     println!("Found {} files with the provided file extensions", result_number);
     println!("Found {} compressed files", compress_files_number);
     println!("File paths saved to output.txt");
